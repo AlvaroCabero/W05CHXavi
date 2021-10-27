77694549917d7e5b12adf68be47cdb538e8d1e60
Alvaro Cabero <alvarocabero@gmail.com>
2021-10-27 13:23:49 +0200

    Added Context...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useContext } from "react";
 import "./App.css";
+import Button from "./components/Button/Button";
 import Display from "./components/Display/Display";
+import operacionContext from "./context/context";
 
 function App() {
-  return <Display dato="42" />;
+  const { displayData, add, subs } = useContext(operacionContext);
+  console.log(displayData);
+
+  return (
+    <>
+      <Display dato={displayData} />
+      <Button text="-" actionOnClick={subs} />
+      <Button text="+" actionOnClick={add} />
+    </>
+  );
 }
 
 export default App;
